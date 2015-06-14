d6d98ae7e0aa9f281dac0f993f18afcab67e7207
Steve Milner <stevem@gnulinux.net>
2015-06-14 11:32:21 -0400

    Fixed index error in middleware.

--- middleware.go
+++ middleware.go
@@ -11,7 +11,7 @@ func RestrictByIP(handler http.HandlerFunc, allowedIps []string) http.HandlerFun
 
 	wrapper := func(w http.ResponseWriter, r *http.Request) {
 		index := sort.SearchStrings(allowedIps, r.RemoteAddr)
-		if allowedIps[index] == r.RemoteAddr {
+		if len(allowedIps) > 0 && allowedIps[index] == r.RemoteAddr {
 			handler(w, r)
 		} else {
 			Logger.Warnf("%s was not in the list of allowed IP's for %v", r.RemoteAddr, handler)
